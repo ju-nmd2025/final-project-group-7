530c391882b80fe3b6cf8e17fdd386b51bd29e00
Adaderib07 <adad25nk@student.ju.se>
2025-12-05 15:01:02 +0100

    Fix merge

--- platform.js
+++ platform.js
@@ -1,13 +1,21 @@
-export let platform = {
-    x: 250,
-    y: 230,
-    w: 80,
-    h: 20,
+export default class Platform {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.w = 80;
+    this.h = 15;
+    this.type = type; // "unbreakable", "breakable", "black", "moving"
+    this.broken = false;
+    this.dx = type === "moving" ? random(1, 3) : 0; // moving speed
+  }
 
-    draw() {
-        push();
-        fill("blue");
-        rect(this.x, this.y, this.w, this.h);
-        pop();
-    },
-};
+  draw() {
+    if (this.type === "unbreakable") fill(150); // grey
+    else if (this.type === "breakable") fill(0, 200, 0); // green
+    else if (this.type === "black") fill(0); // black
+    else fill(0, 200, 0); // moving green
+
+    rectMode(CENTER);
+    rect(this.x, this.y, this.w, this.h, 5);
+  }
+}
