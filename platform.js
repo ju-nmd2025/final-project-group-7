5db112ffb487420a908e6f2328516407141ae44e
emandiem <diem25jn@student.ju.se>
2025-12-10 16:02:23 +0100

    Edited code

--- platform.js
+++ platform.js
@@ -4,16 +4,21 @@ export default class Platform {
     this.y = y;
     this.w = 80;
     this.h = 15;
-    this.type = type; // "unbreakable", "breakable", "black", "moving"
+    this.type = type; // "unbreakable", "black", "moving"
     this.broken = false;
     this.dx = type === "moving" ? random(1, 3) : 0; // moving speed
   }
 
   draw() {
-    if (this.type === "unbreakable") fill(150); // grey
-    else if (this.type === "breakable") fill(0, 200, 0); // green
-    else if (this.type === "black") fill(0); // black
-    else fill(0, 200, 0); // moving green
+    if (this.type === "unbreakable") {
+      fill(150); // grey
+    } else if (this.type === "black") {
+      fill(0); // black (breakable)
+    } else if (this.type === "moving") {
+      fill(0, 200, 0); // green moving
+    } else {
+      fill(150); // fallback
+    }
 
     rectMode(CENTER);
     rect(this.x, this.y, this.w, this.h, 5);
